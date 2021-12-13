7c0493e5709674beece731b3d6b48f4ccdaa4f25
nmel88 <81481184+nmel88@users.noreply.github.com>
2021-12-13 14:44:20 -0700

    Re-added Doctors and Students

---
+++ Webservice/Controllers/DoctorsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BusinessLibrary.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Webservice.ContextHelpers;
+using Webservice.ControllerHelpers;
+using Newtonsoft.Json.Linq;
+
+namespace Webservice.Controllers
+{
+    [Route("api/doctors")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+
+        #region Initialization
+
+        /// <summary>
+        /// Reference to the hosting environment instance added in the Startup.cs.
+        /// </summary>
+        private readonly IHostingEnvironment HostingEnvironment;
+
+        /// <summary>
+        /// Reference to the app settings helper instance added in the Startup.cs.
+        /// </summary>
+        private readonly AppSettingsHelper AppSettings;
+
+        /// <summary>
+        /// Reference to the database context helper instance added in the Startup.cs.
+        /// </summary>
+        private readonly DatabaseContextHelper Database;
+
+        /// <summary>
+        /// Constructor called by the service provider.
+        /// Using injection to get the arguments.
+        /// </summary>
+        public DoctorsController(IHostingEnvironment hostingEnvironment, AppSettingsHelper appSettings,
+            DatabaseContextHelper database)
+        {
+            HostingEnvironment = hostingEnvironment;
+            AppSettings = appSettings;
+            Database = database;
+        }
+
+        #endregion
+
+
+        // Gets collection.
+        [HttpGet]
+        [Route("GetDoctors")]
+        public ResponseMessage GetDoctors()
+        {
+            var response = DoctorHelper.GetCollection(
+                context: Database.DbContext,
+                statusCode: out HttpStatusCode statusCode,
+                includeDetailedErrors: HostingEnvironment.IsDevelopment());
+            HttpContext.Response.StatusCode = (int)statusCode;
+            return response;
+        }
+
+        // Adds a new instance.
+        [HttpPost]
+        [Route("AddDoctor")]
+        public ResponseMessage AddDoctor([FromBody] JObject data)
+        {
+
+            var response = DoctorHelper.Add(data,
+                context: Database.DbContext,
+                statusCode: out HttpStatusCode statusCode,
+                includeDetailedErrors: HostingEnvironment.IsDevelopment());
+            HttpContext.Response.StatusCode = (int)statusCode;
+            return response;
+        }
+
+    }
+}

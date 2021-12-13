231c2dca9d0695deedeb4604dfc2c01e6c23867a
Nuha <nuha.shaikh@gmail.com>
2021-12-12 17:02:56 -0700

    clinic, medication, works_at, side_effect model + helper APIs

---
+++ Webservice/ControllerHelpers/ClinicHelper.cs
@@ -0,0 +1,93 @@
+using BusinessLibrary.Models;
+using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Webservice.ControllerHelpers
+{
+    public class ClinicHelper
+    {
+
+        #region Converters
+
+        /// <summary>
+        /// Converts database models to a business logic object.
+        /// </summary>
+        public static BusinessLibrary.Models.Doctor Convert(Doctor_db instance)
+        {
+            if (instance == null)
+                return null;
+            return new BusinessLibrary.Models.Doctor(instance.Id, instance.Name, instance.Password);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Signs up a student.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Add(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            int id = (data.ContainsKey("id")) ? data.GetValue("id").Value<int>() : 0;
+            string name = (data.ContainsKey("name")) ? data.GetValue("name").Value<string>() : null;
+            string password = (data.ContainsKey("password")) ? data.GetValue("password").Value<string>() : null;
+
+            // Add instance to database
+            var dbInstance = DatabaseLibrary.Helpers.DoctorHelper_db.Add(id, name, password,
+                context, out StatusResponse statusResponse);
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while adding a new student.";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    dbInstance != null,
+                    statusResponse.Message,
+                    Convert(dbInstance)
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+        /// <summary>
+        /// Gets list of students.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage GetCollection(
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Get instances from database
+            var dbInstances = DatabaseLibrary.Helpers.DoctorHelper_db.GetCollection(
+                context, out StatusResponse statusResponse);
+
+            // Convert to business logic objects
+            var instances = dbInstances?.Select(x => Convert(x)).ToList();
+
+            // Get rid of detailed error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while retrieving the students";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    instances != null,
+                    statusResponse.Message,
+                    instances
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+    }
+}
